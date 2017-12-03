bdded3c1e3fbed839b17bfda9dc82db9f14d0922
Jakub Duchon <jakub.duchon9@users.noreply.github.com>
2017-12-03 11:16:03 +0100

    Update 3/12/2017

--- QuakeReport/app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ QuakeReport/app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -1,6 +1,8 @@
 package com.example.android.quakereport;
 
 
+import static com.example.android.quakereport.R.id.date;
+
 /**
  * Created by jakub on 10/12/2017.
  */
@@ -9,21 +11,27 @@ public class Earthquake{
 
     private double magnitude;
     private String location;
-    private String date;
+    private long timeInMilliseconds;
+    private String url;
 
-    public Earthquake(double magnitude, String location, String date){
+    public Earthquake(double magnitude, String location, long timeInMilliseconds, String url){
         this.magnitude = magnitude;
         this.location = location;
-        this.date = date;
+        this.timeInMilliseconds = timeInMilliseconds;
+        this.url = url;
     }
 
     public double getMagnitude(){
         return magnitude;
     }
 
+    public String getUrl(){
+        return url;
+    }
+
     public String getLocation(){
         return location;
     }
 
-    public String getDate() { return date; }
+    public Long getTimeInMilliseconds() { return timeInMilliseconds; }
 }
